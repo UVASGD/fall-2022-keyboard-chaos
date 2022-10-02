cf12e8fc3164e1cfc9b6a923917f8eaaa768e063
JoeyGiordano <jgzgiordano@gmail.com>
2022-10-02 15:07:48 -0400

    Another quick fix

--- Assets/Scripts/CamSmoothing.cs
+++ Assets/Scripts/CamSmoothing.cs
@@ -16,6 +16,7 @@ public class CamSmoothing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.rotation.SetEulerAngles(gameObject.transform.rotation.eulerAngles + Time.deltaTime * 2 * (camSmoother.transform.rotation.eulerAngles - gameObject.transform.rotation.eulerAngles));
+        Vector3 deltaInEuler = gameObject.transform.rotation.eulerAngles + Time.deltaTime * 2 * (camSmoother.transform.rotation.eulerAngles - gameObject.transform.rotation.eulerAngles);
+        gameObject.transform.rotation.eulerAngles.Set(deltaInEuler.x, deltaInEuler.y, deltaInEuler.z);
     }
 }
