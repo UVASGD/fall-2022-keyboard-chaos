ea9c45dbcc0cf804cc71b0064cffdf2406f2519d
Junho Lee <junholee6a@gmail.com>
2022-11-17 08:35:47 -0500

    Make SphereEnemy able to attack with fire spells

--- Assets/Scripts/Enemies/SphereEnemy.cs
+++ Assets/Scripts/Enemies/SphereEnemy.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class SphereEnemy : Enemy
 {
     public Player player;
+    [SerializeField] GameObject defaultSpellBallPrefab;
     Ability fireSpell;
+    float range;
 
     // Start is called before the first frame update
     void Start()
     {
         hitPoints = maxHealth;
         alive = true;
+        fireSpell = new Ability("fireSpell", gameObject, 0, 5, "");
+        range = 15;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // put attack code here later
+        bool inRange = Vector3.Distance(transform.position, player.gameObject.transform.position) <= range;
+
+        // When in range, enemy should face the player.
+        // This is used for aiming the projectile
+        if (inRange && !isDizzy)
+            transform.LookAt(player.transform);
+
+        if (alive && inRange && !isDizzy && fireSpell.tryUseAbility(player)) {
+            GameObject tempSpellBall = Instantiate(defaultSpellBallPrefab, transform.position + transform.forward + transform.up, transform.rotation);
+            tempSpellBall.GetComponent<Rigidbody>().velocity = transform.forward * 10;
+        }
     }
 
     public override void Die() {
